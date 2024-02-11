9c0db20727be6b17580fbafedae59f367c496865
Baibhab Ghosh <140502166+UzumakiBaibhab@users.noreply.github.com>
2024-02-11 21:25:50 +0530

    Delete Generic Questions.sql

--- Generic Questions.sql
+++
@@ -1,6 +0,0 @@
--- -------------------------------- GENERIC QUESTIONS ----------------------------------
--- How many unique cities does the data have?
-select distinct city from sales;
--- In which city is each branch?
-select distinct city,branch from sales;
--- -------------------------------------------------------------------------------------
